e20a9510f756d7d8c91196d9f6a578bde4b6d081
brinkbrink <brinkbrink@gmail.com>
2022-03-26 08:26:55 -0700

    init

--- budgetbuddy/settings.py
+++ budgetbuddy/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ju^p9760f)x-i33m_&kd0+1cywmvsu*ht2fb1h4&6tevb5cj&0'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
